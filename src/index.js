4afc60fd0e5cde72b8d6d56ee6956de921804afe
Ben Wilson <benjaminw1030@gmail.com>
2021-09-03 14:00:40 -0700

    full UI functionality

--- src/index.js
+++ src/index.js
@@ -4,15 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Calculator from './js/calculator.js';
 
-$(document).ready(function() {
-  $('#calculator-form').submit(function(event) {
+$(document).ready(function () {
+  $('#calculator-form').submit(function (event) {
     event.preventDefault();
     const currentDate = Date.now();
     const birthday = $("#birthday").val();
     const planet = $("#planet").val();
     const lifeExpectancy = $("#life-expectancy").val();
-    const age = new Calculator(birthday);
-    $('#result').fadeIn();
-    $('#ytl-output').text(age.yearsToLiveOutput(planet, lifeExpectancy, currentDate));
+    const bDayObj = new Calculator(birthday);
+    let age;
+    switch (planet) {
+      case "Earth":
+        age = bDayObj.earthAge(currentDate);
+        break;
+      case "Mercury":
+        age = bDayObj.mercuryAge(currentDate);
+        break;
+      case "Venus":
+        age = bDayObj.venusAge(currentDate);
+        break;
+      case "Mars":
+        age = bDayObj.marsAge(currentDate);
+        break;
+      case "Jupiter":
+        age = bDayObj.jupiterAge(currentDate);
+    }
+    $("#result").fadeIn();
+    $("#age-output").text(`you are ${age} ${planet} years old.`)
+    $("#ytl-output").text(bDayObj.yearsToLiveOutput(planet, lifeExpectancy, currentDate));
   });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -2,26 +2,26 @@ export default class Calculator {
   constructor(date) {
     this.date = date;
   }
-  
+
   earthAge(currentDate) {
     const birthday = new Date(this.date);
     return ((currentDate - birthday) / 31536000000);
   }
 
   mercuryAge(currentDate) {
-    return (this.earthAge(currentDate)/.24);
+    return (this.earthAge(currentDate) / .24);
   }
 
   venusAge(currentDate) {
-    return (this.earthAge(currentDate)/.62);
+    return (this.earthAge(currentDate) / .62);
   }
 
   marsAge(currentDate) {
-    return (this.earthAge(currentDate)/1.88);
+    return (this.earthAge(currentDate) / 1.88);
   }
 
   jupiterAge(currentDate) {
-    return (this.earthAge(currentDate)/11.86);
+    return (this.earthAge(currentDate) / 11.86);
   }
 
   yearsToLive(planet, lifeExpectancy, currentDate) {
@@ -29,19 +29,19 @@ export default class Calculator {
       case "Earth":
         return lifeExpectancy - this.earthAge(currentDate);
       case "Mercury":
-        return lifeExpectancy/.24 - this.mercuryAge(currentDate);
+        return lifeExpectancy / .24 - this.mercuryAge(currentDate);
       case "Venus":
-        return lifeExpectancy/.62 - this.venusAge(currentDate);
+        return lifeExpectancy / .62 - this.venusAge(currentDate);
       case "Mars":
-        return lifeExpectancy/1.88 - this.marsAge(currentDate);
+        return lifeExpectancy / 1.88 - this.marsAge(currentDate);
       case "Jupiter":
-        return lifeExpectancy/11.86 - this.jupiterAge(currentDate);
+        return lifeExpectancy / 11.86 - this.jupiterAge(currentDate);
     }
   }
 
   yearsToLiveOutput(planet, lifeExpectancy, currentDate) {
     const years = this.yearsToLive(planet, lifeExpectancy, currentDate);
-    const yearsOutput = Math.abs(years).toFixed(2)
+    const yearsOutput = Math.abs(years).toFixed(2);
     if (years < 0) {
       return `You have lived ${yearsOutput} years past your expected lifespan on ${planet}. Keep it up!`;
     } else {
