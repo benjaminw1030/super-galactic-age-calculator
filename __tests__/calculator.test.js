9c288fe0ef98340cdb2084628dbdd1960754bc3f
Ben Wilson <benjaminw1030@gmail.com>
2021-09-03 15:10:04 -0700

    refactor DRY up code

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,15 +3,17 @@ import Calculator from '../src/js/calculator.js';
 describe('Calculator', () => {
 
   let calculator;
-  let currentDate;
-   
+
   beforeEach(() => {
-    calculator = new Calculator("1984-10-30", "Earth");
-    currentDate = 1630685647903; //freezes current date with value of Date.now() on 9/3/2021.
+    calculator = new Calculator("1984-10-30", "Earth", 1630685647903, 80);
+    //freezes current date with value of Date.now() on 9/3/2021.
   });
 
   test('should create a calculator object', () => {
-    expect(calculator.date).toEqual("1984-10-30");
+    expect(calculator.birthday).toEqual("1984-10-30");
+    expect(calculator.planet).toEqual("Earth");
+    expect(calculator.currentDate).toEqual(1630685647903);
+    expect(calculator.lifeExpectancy).toEqual(80);
   });
 
   test('should calculate an orbital period based on the planet', () => {
@@ -33,43 +35,43 @@ describe('Calculator', () => {
   });
 
   test('should calculate Earth age from input', () => {
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(36.87);
+    expect(calculator.planetAge()).toBeCloseTo(36.87);
   });
 
   test('should calculate age on any planet from input', () => {
     calculator.planet = "Mercury";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(153.63);
+    expect(calculator.planetAge()).toBeCloseTo(153.63);
     calculator.planet = "Venus";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(59.47);
+    expect(calculator.planetAge()).toBeCloseTo(59.47);
     calculator.planet = "Mars";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(19.61);
+    expect(calculator.planetAge()).toBeCloseTo(19.61);
     calculator.planet = "Jupiter";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(3.11);
+    expect(calculator.planetAge()).toBeCloseTo(3.11);
     calculator.planet = "Saturn";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(1.25);
+    expect(calculator.planetAge()).toBeCloseTo(1.25);
     calculator.planet = "Uranus";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(0.44);
+    expect(calculator.planetAge()).toBeCloseTo(0.44);
     calculator.planet = "Neptune";
-    expect(calculator.planetAge(currentDate)).toBeCloseTo(0.22);
+    expect(calculator.planetAge()).toBeCloseTo(0.22);
   });
 
   test('should calculate years to live on Earth from input', () => {
-    expect(calculator.yearsToLive("Earth", 80, currentDate)).toBeCloseTo(43.13);
+    expect(calculator.yearsToLive()).toBeCloseTo(43.13);
   });
 
   test('should calculate years to live on any planet from input', () => {
-    expect(calculator.yearsToLive("Mercury", 80, currentDate)).toBeCloseTo(179.71);
-    expect(calculator.yearsToLive("Venus", 80, currentDate)).toBeCloseTo(69.56);
-    expect(calculator.yearsToLive("Mars", 80, currentDate)).toBeCloseTo(22.94);
-    expect(calculator.yearsToLive("Jupiter", 80, currentDate)).toBeCloseTo(3.64);
+    calculator.planet = "Mercury";
+    expect(calculator.yearsToLive()).toBeCloseTo(179.71);
+    calculator.planet = "Jupiter"
+    expect(calculator.yearsToLive()).toBeCloseTo(3.64);
   });
 
   test('should output a string describing how many years a person has left to live', () => {
-    expect(calculator.yearsToLiveOutput("Earth", 80, currentDate)).toEqual("You have 43.13 years left to live on Earth. Make the most of it!");
+    expect(calculator.yearsToLiveOutput()).toEqual("You have 43.13 years left to live on Earth. Make the most of it!");
   });
 
   test('should output a special string describing how many years a person has lived past their life expectancy', () => {
-    calculator = new Calculator("1904-10-30");
-    expect(calculator.yearsToLiveOutput("Earth", 80, currentDate)).toEqual("You have lived 36.93 years past your expected lifespan on Earth. Keep it up!");
+    calculator.birthday = "1904-10-30";
+    expect(calculator.yearsToLiveOutput()).toEqual("You have lived 36.93 years past your expected lifespan on Earth. Keep it up!");
   });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,7 +1,9 @@
 export default class Calculator {
-  constructor(date, planet) {
-    this.date = date;
+  constructor(birthday, planet, currentDate, lifeExpectancy) {
+    this.birthday = birthday;
     this.planet = planet;
+    this.currentDate = currentDate;
+    this.lifeExpectancy = lifeExpectancy;
   }
 
   orbitalPeriod() {
@@ -25,50 +27,23 @@ export default class Calculator {
     }
   }
 
-
-  planetAge(currentDate) {
-    const birthday = new Date(this.date);
-    return ((currentDate - birthday) / 31536000000) / this.orbitalPeriod();
+  planetAge() {
+    const birthdayObj = new Date(this.birthday);
+    const birthdayNum = birthdayObj.valueOf();
+    return ((this.currentDate - birthdayNum) / 31536000000) / this.orbitalPeriod();
+  }
+ 
+  yearsToLive() {
+      return (this.lifeExpectancy/this.orbitalPeriod()) - this.planetAge(this.currentDate);
   }
 
-  mercuryAge(currentDate) {
-    return (this.earthAge(currentDate) / .24);
-  }
-
-  venusAge(currentDate) {
-    return (this.earthAge(currentDate) / .62);
-  }
-
-  marsAge(currentDate) {
-    return (this.earthAge(currentDate) / 1.88);
-  }
-
-  jupiterAge(currentDate) {
-    return (this.earthAge(currentDate) / 11.86);
-  }
-
-  yearsToLive(planet, lifeExpectancy, currentDate) {
-    switch (planet) {
-      case "Earth":
-        return lifeExpectancy - this.planetAge(currentDate);
-      case "Mercury":
-        return lifeExpectancy / .24 - this.planetAge(currentDate);
-      case "Venus":
-        return lifeExpectancy / .62 - this.planetAge(currentDate);
-      case "Mars":
-        return lifeExpectancy / 1.88 - this.marsAge(currentDate);
-      case "Jupiter":
-        return lifeExpectancy / 11.86 - this.jupiterAge(currentDate);
-    }
-  }
-
-  yearsToLiveOutput(planet, lifeExpectancy, currentDate) {
-    const years = this.yearsToLive(planet, lifeExpectancy, currentDate);
+  yearsToLiveOutput() {
+    const years = this.yearsToLive();
     const yearsOutput = Math.abs(years).toFixed(2);
     if (years < 0) {
-      return `You have lived ${yearsOutput} years past your expected lifespan on ${planet}. Keep it up!`;
+      return `You have lived ${yearsOutput} years past your expected lifespan on ${this.planet}. Keep it up!`;
     } else {
-      return `You have ${yearsOutput} years left to live on ${planet}. Make the most of it!`;
+      return `You have ${yearsOutput} years left to live on ${this.planet}. Make the most of it!`;
     }
   }
 }
