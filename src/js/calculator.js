ef2c3c663eae02ba415a4b28fb7b6efe334f1dff
Ben Wilson <benjaminw1030@gmail.com>
2021-09-03 10:37:31 -0700

    good fail for yearsToLive for other planets

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -27,7 +27,10 @@ export default class Calculator {
   }
 
   yearsToLive(planet, lifeExpectancy, currentDate) {
-    return lifeExpectancy - this.earthAge(currentDate); 
+    switch (planet) {
+      case "Earth":
+        return lifeExpectancy - this.earthAge(currentDate);
+    }
   }
 }
 
